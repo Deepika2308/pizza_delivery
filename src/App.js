7d1e1cda18c9ebad3f01744c3ea6c4e0d2b12276
Deepika2308 <deepika.vijai@gmail.com>
2022-04-03 02:49:47 +0530

    Hackathon_2_signup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import {Link, Route} from "react-router-dom";
+import {useState} from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage />
     </div>
   );
 }
 
-export default App;
+let USERS_LIST = [
+  {
+    id:"100",
+    name:"Jakie",
+    password:"pass@123",
+    emailId:"jack@gmail.com"
+  }
+]
+
+function HomePage(){
+  return (
+
+    <div className="top-nav">
+        <ul>
+          <li>
+            <Link to="/signup">Signup</Link>
+           
+          </li>
+          <li>
+            
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+
+        <switch>
+          <Route path="/signup">
+            <SignupPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+        </switch>
+    </div>
+  )
+}
+
+function SignupPage(){
+
+  let[name,setName] = useState("");
+  let[password,setPassword] = useState("");
+  let[emailId,setMail] = useState("");
+  let[usersList,setUsersList] = useState(USERS_LIST);
+
+  return(
+   <div className="signup-form">
+    <input id="name" placeholder="User name" onChange={(event) => setName(event.target.value) } ></input>
+    <input id="password" placeholder="Password" onChange={(event) => setPassword(event.target.value) }></input>
+    <input id="emailId" placeholder="Email id" onChange={(event) => setMail(event.target.value) }></input>
+    <button onClick={() => {
+      console.log(name);
+      console.log(password);
+      console.log(emailId);
+      let newUser={
+        name:name,
+        password:password,
+        emailId:emailId
+      };
+
+      fetch("https://62488f87831c69c687c5b15d.mockapi.io/users",{
+        method:"POST",
+        body: JSON.stringify(newUser),
+        headers:{
+          "content-type": "application/json",
+        }
+      });
+      // setUsersList([...usersList,newUser]);
+      console.log("users list " +usersList);
+    } }>Signup</button>
+   </div> 
+  )
+}
+
+function LoginPage(){
+  return(
+   <div className="login-form">
+    <input id="loginUserName" placeholder="User name"></input>
+    <input id="loginPassword" placeholder="Password"></input>
+    <button>Login</button>
+   </div> 
+  )
+}
